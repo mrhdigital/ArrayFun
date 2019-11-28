04c807a0b31776bb464b97ee3f67033de65fd5dd
unknown <mrhdigital@yahoo.com>
2019-11-28 16:36:09 -0600

    use Array.prototype.every()

--- assets/javascript/arrayFun.js
+++ assets/javascript/arrayFun.js
@@ -165,6 +165,11 @@ console.log(totalYears);
 
     console.log({isAdult});
 
+    const allAdult = people1.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+    console.log({allAdult});
+
+
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
