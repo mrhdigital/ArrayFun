43ff871fda3d50f6fbadb6bacd561b67fd84c1f4
unknown <mrhdigital@yahoo.com>
2019-12-11 21:02:20 -0600

    find the if secret code in the user typed keys

--- assets/javascript/arrayFun.js
+++ assets/javascript/arrayFun.js
@@ -209,6 +209,7 @@ const secretCode = 'mrh'
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
   pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   console.log(pressed);
 })
 
