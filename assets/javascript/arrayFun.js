2f0ed925b5d35d8354151d784c26434e25314f8d
unknown <mrhdigital@yahoo.com>
2019-12-11 21:25:23 -0600

    cornify_add();

--- assets/javascript/arrayFun.js
+++ assets/javascript/arrayFun.js
@@ -204,18 +204,18 @@ function deleteFunction() {
 
 console.table(newComments);
 
-const pressed =[];
-const secretCode = 'mrh'
+const pressed = [];
+const secretCode = 'happy';
+
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  console.log(pressed);
-  if(pressed.join('').includes(secretCode)) {
-    console.log('Ding Ding');
+  if (pressed.join('').includes(secretCode)) {
+    console.log('DING DING!');
+    cornify_add();
+
   }
-})
 
-
-
-
+  console.log(pressed);
+});
